c4a02ecd3127431d45cddbd347206e45dc0d73d6
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-08 14:21:53 -0700

    chart

---
+++ 2_Basics_and_Components/React_Expense_Tracker/components/Chart/ChartBar.js
@@ -0,0 +1,25 @@
+import "./ChartBar.css";
+
+const ChartBar = (props) => {
+  //Initial value for the bar filling css
+  let barFillHeight = "0%";
+
+  //Set the hight filling css based on what is passed
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
