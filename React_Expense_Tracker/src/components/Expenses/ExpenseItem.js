0ccba3aff67519acf477aaaa96f443f2e322f0df
filipecamargos <filipe.atrabalho@gmail.com>
2022-01-15 15:00:57 -0700

    multiple expenses item using props

--- React_Expense_Tracker/src/components/Expenses/ExpenseItem.js
+++ React_Expense_Tracker/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,44 @@
-import React, {useReducer, useState} from 'react';
+import React, { useState } from 'react';
 
-import ExpenseDate from './ExpenseDate';
+import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
-const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  //Handle the btn click
-  const clickHandler = () => {
-    setTitle("Updated!")
-  }
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
-export default ExpenseItem;
+export default Expenses;
